d79c99bbe23d73410df7983fac79457eb723e434
RyanDevGitHub <ryanhaddad12@gmail.com>
2024-06-11 19:44:54 +0200

    html PageAccount

--- src/app/pages/account/account/account.page.ts
+++ src/app/pages/account/account/account.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol, IonRow, IonAvatar, IonText, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
   standalone: true,
-  imports: [IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonCol,IonRow]
+  imports: [IonButton, IonText, IonAvatar, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonCol,IonRow]
 })
 export class AccountPage implements OnInit {
 
@@ -17,4 +17,7 @@ export class AccountPage implements OnInit {
   ngOnInit() {
   }
 
+  editProfile(){
+
+  }
 }
