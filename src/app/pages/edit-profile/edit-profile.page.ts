c572f08af3a01c394c7d6864054056c1215a8a9b
RyanDevGitHub <ryanhaddad12@gmail.com>
2024-06-27 14:10:34 +0200

    edit profile

--- src/app/pages/edit-profile/edit-profile.page.ts
+++ src/app/pages/edit-profile/edit-profile.page.ts
@@ -2,21 +2,34 @@ import { MusicContainerComponent } from './../../shared/components/music-contain
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 import { PlaylistContainerComponent } from 'src/app/shared/components/playlist-container/playlist-container.component';
+import { HeaderCategoryComponent } from 'src/app/shared/components/header-category/header-category.component';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.page.html',
   styleUrls: ['./edit-profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,MusicContainerComponent,PlaylistContainerComponent]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    MusicContainerComponent,
+    PlaylistContainerComponent,
+    HeaderCategoryComponent,
+  ],
 })
 export class EditProfilPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
