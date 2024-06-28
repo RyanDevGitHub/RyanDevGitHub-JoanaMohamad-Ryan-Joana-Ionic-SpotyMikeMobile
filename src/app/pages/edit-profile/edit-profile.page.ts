f3079c013bc658071d93c37738a8100e79531fb4
Joana Mohamad <Joanamohamad10@gmail.com>
2024-06-28 10:26:22 +0200

    edit profile page users done

--- src/app/pages/edit-profile/edit-profile.page.ts
+++ src/app/pages/edit-profile/edit-profile.page.ts
@@ -1,13 +1,24 @@
-import { MusicContainerComponent } from './../../shared/components/music-container/music-container.component';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonicComponentsModule } from '../../shared/ionic-components.module'; 
 import {
   IonContent,
   IonHeader,
   IonTitle,
   IonToolbar,
-} from '@ionic/angular/standalone';
+  IonAvatar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButtons,
+  IonButton,
+  IonBackButton,
+  IonItem,
+  IonLabel,
+  IonInput,
+} from '@ionic/angular';
+import { MusicContainerComponent } from './../../shared/components/music-container/music-container.component';
 import { PlaylistContainerComponent } from 'src/app/shared/components/playlist-container/playlist-container.component';
 import { HeaderCategoryComponent } from 'src/app/shared/components/header-category/header-category.component';
 
@@ -17,12 +28,23 @@ import { HeaderCategoryComponent } from 'src/app/shared/components/header-catego
   styleUrls: ['./edit-profile.page.scss'],
   standalone: true,
   imports: [
+    CommonModule,
+    FormsModule,
+    IonicComponentsModule, 
     IonContent,
     IonHeader,
     IonTitle,
     IonToolbar,
-    CommonModule,
-    FormsModule,
+    IonAvatar,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonButtons,
+    IonButton,
+    IonBackButton,
+    IonItem,
+    IonLabel,
+    IonInput,
     MusicContainerComponent,
     PlaylistContainerComponent,
     HeaderCategoryComponent,
---
+++ src/app/shared/ionic-components.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  exports: [IonicModule],
+})
+export class IonicComponentsModule {}
